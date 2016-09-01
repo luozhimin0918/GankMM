2b2c53a16cfb8f990cda4c83d737c14221dd6d2d
maning <ning.ma@12301.cn>
2016-09-01 11:33:17 +0800

    删除无用代码

--- app/src/main/java/com/maning/gankmm/ui/activity/MainActivity.java
+++ app/src/main/java/com/maning/gankmm/ui/activity/MainActivity.java
@@ -10,7 +10,6 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.MenuItem;
 
 import com.afollestad.materialdialogs.MaterialDialog;
